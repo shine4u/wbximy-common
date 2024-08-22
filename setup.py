7baa8a71efb543799581eee1b8c11ad5ba743c95
wangbangxu <wangbangxu@tianyancha.com>
2024-08-22 19:31:13 +0800

    update

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wbximy_common',
+    version='0.0.1',
+    description='common tools wbximy.',
+    author='WangBangXu',
+    author_email='shine19890705@gmail.com',
+    url='https://github.com/shine4u/wbximy-common',
+    packages=find_packages(),
+    install_requires=[
+        # List your library's dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
