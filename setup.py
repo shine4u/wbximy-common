0042e7b8de2c5a1630d37bc6db40e8e52069c50e
wangbangxu <wangbangxu@tianyancha.com>
2024-09-25 16:03:34 +0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wbximy_common',
-    version='0.0.1',
+    version='0.0.2',
     description='common tools wbximy.',
     author='WangBangXu',
     author_email='shine19890705@gmail.com',
